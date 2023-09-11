74b0eb47a14e24c207286df2766cb2a58ff77eeb
Shinichi Nakagawa <itchyshin@gmail.com>
2023-09-11 11:14:29 +0900

    Update zoo2.R

--- R/zoo2.R
+++ R/zoo2.R
@@ -392,12 +392,176 @@ orchard_plot(mod_comb, mod = "category",
             xlab = "lnRR (all)", group = "vertlife.species", 
             g = FALSE, angle = 45)
 
+res <- all_models(mod_comb, mod = "category")  
+
 # creating a table to show contrasts between categories
 
 ############
 # wriring a function to get the contrasts
 ############
 
+# custom functions
+
+#' Title: Contrast name generator
+#'
+#' @param name: a vector of character strings
+cont_gen <- function(name) {
+  combination <- combn(name, 2)
+  name_dat <- t(combination)
+  names <- paste(name_dat[, 1], name_dat[, 2], sep = "-")
+  return(names)
+}
+
+#' @title get_pred1: intercept-less model
+#' @description Function to get CIs (confidence intervals) and PIs (prediction intervals) from rma objects (metafor)
+#' @param model: rma.mv object 
+#' @param mod: the name of a moderator 
+get_pred1 <- function (model, mod = " ") {
+  name <- firstup(as.character(stringr::str_replace(row.names(model$beta), mod, "")))
+  len <- length(name)
+  
+   if (len != 1) {
+        newdata <- diag(len)
+        pred <- metafor::predict.rma(model, 
+                                     newmods = newdata,
+                                     tau2.levels = 1:len)
+    }
+    else {
+        pred <- metafor::predict.rma(model)
+  }
+  estimate <- pred$pred
+  lowerCL <- pred$ci.lb
+  upperCL <- pred$ci.ub 
+  lowerPR <- pred$cr.lb
+  upperPR <- pred$cr.ub 
+  
+  table <- tibble(name = factor(name, levels = name, labels = name), estimate = estimate,
+                  lowerCL = lowerCL, upperCL = upperCL,
+                  pval = model$pval,
+                  lowerPR = lowerPR, upperPR = upperPR)
+}
+
+#' @title get_pred2: normal model
+#' @description Function to get CIs (confidence intervals) and PIs (prediction intervals) from rma objects (metafor)
+#' @param model: rma.mv object 
+#' @param mod: the name of a moderator 
+get_pred2 <- function (model, mod = " ") {
+  name <- as.factor(str_replace(row.names(model$beta), 
+                                paste0("relevel", "\\(", mod,", ref = name","\\)"),""))
+  len <- length(name)
+  
+  if(len != 1){
+  newdata <- diag(len)
+  pred <- predict.rma(model, intercept = FALSE, newmods = newdata[ ,-1])
+  }
+  else {
+    pred <- predict.rma(model)
+  }
+  estimate <- pred$pred
+  lowerCL <- pred$ci.lb
+  upperCL <- pred$ci.ub 
+  lowerPR <- pred$cr.lb
+  upperPR <- pred$cr.ub 
+  
+  table <- tibble(name = factor(name, levels = name, labels = name), estimate = estimate,
+                  lowerCL = lowerCL, upperCL = upperCL,
+                  pval = model$pval,
+                  lowerPR = lowerPR, upperPR = upperPR)
+}
+
+#' @title mr_results
+#' @description Function to put results of meta-regression and its contrasts
+#' @param res1: data frame 1
+#' @param res1: data frame 2
+mr_results <- function(res1, res2) {
+  restuls <-tibble(
+    `Fixed effect` = c(as.character(res1$name), cont_gen(res1$name)),
+    Estimate = c(res1$estimate, res2$estimate),
+    `Lower CI [0.025]` = c(res1$lowerCL, res2$lowerCL),
+    `Upper CI  [0.975]` = c(res1$upperCL, res2$upperCL),
+    `P value` = c(res1$pval, res2$pval),
+    `Lower PI [0.025]` = c(res1$lowerPR, res2$lowerPR),
+    `Upper PI  [0.975]` = c(res1$upperPR, res2$upperPR),
+  )
+}
+
+
+#' @title all_models
+#' @description Function to take all possible models and get their results
+#' @param model: intercept-less model
+#' @param mod: the name of a moderator 
+
+all_models <- function(model, mod = " ", type = "homo") {
+  
+  # getting the level names out
+  level_names <- levels(factor(model$data[[mod]]))
+  dat2 <- model$data
+  mod <- mod
+
+
+  #model$data[[mod]] <- factor(model$data[[mod]], ordered = FALSE)
+  # meta-regression: contrasts 
+  # helper function to run metafor meta-regression
+  run_rma1 <- function(name) {
+      VCV1 <- vcalc(vi = dat2$vi,
+             cluster = dat2$vertlife.species,
+             rho = 0.5)
+    rma.mv(yi, V = VCV1,
+                   mods = ~relevel(dat2[[mod]], ref = name),
+                     random = list(
+                       ~1|vertlife.species,
+                       ~1|phylogeny,
+                       ~1|obs_id),
+                     R = list(phylogeny = cor_tree),
+                     data = dat2,
+                     control = list(optimizer = "Nelder-Mead"))
+   }
+
+    run_rma2 <- function(name) {
+    
+            VCVa <- vcalc(abs_vi, vertlife.species, 
+                    rho = 0.5, data = dat2)
+               
+               rma.mv(abs_yi, V = VCVa,
+               mods = ~relevel(dat2[[mod]], ref = name),
+                     random = list(
+                       ~1|vertlife.species,
+                       ~1|phylogeny,
+                       ~1|obs_id),
+                     R = list(phylogeny = cor_tree),
+                     data = dat2,
+                     control = list(optimizer = "Nelder-Mead"))
+   }
+
+# results of meta-regression including all contrast results; taking the last level out ([-length(level_names)])
+# this does not work for hetero model?
+if (type == "homo"){
+
+    model_all <- purrr::map(level_names[-length(level_names)], run_rma1)
+
+  } else {
+  model_all <- purrr::map(level_names[-length(level_names)], run_rma2)
+  }
+  
+  # getting estimates from intercept-less models (means for all the groups)
+  res1 <- get_pred1(model, mod = mod)
+  
+  # getting estiamtes from all contrast models
+  res2_pre <- purrr::map(model_all, ~ get_pred2(.x, mod = mod))
+  
+  # a list of the numbers to take out unnecessary contrasts
+  contra_list <- Map(seq, from=1, to=1:(length(level_names) - 1))
+  res2 <- purrr::map2_dfr(res2_pre, contra_list, ~.x[-(.y), ]) 
+  # creating a table
+  res_tab <- mr_results(res1, res2) %>% 
+  kable("html",  digits = 3) %>%
+  kable_styling("striped", position = "left") %>%
+  scroll_box(width = "100%")
+  
+  # results
+  res_tab
+
+}
 
 
 ##########
@@ -432,7 +596,8 @@ dat_comb <- dat_comb %>% mutate(
 
 dat_comb[which(dat_comb$abs_yi == max(dat_comb$abs_yi)), ]
 
-hist(log(dat_comb$abs_vi))
+#hist(log(dat_comb$abs_vi))
+
 
 # modeling
 
@@ -449,9 +614,12 @@ mod_comb_a <- rma.mv(abs_yi, V = VCVa,
                      control = list(optimizer = "Nelder-Mead"))
 summary(mod_comb_a)
 r2_ml(mod_comb_a)
+res2 <- all_models(mod_comb_a, mod = "category", type = "abs")
 
 robust(mod_comb_a, cluster = vertlife.species)  
 
 orchard_plot(mod_comb_a, mod = "category",
             xlab = "lnRR (all)", group = "vertlife.species", 
             g = FALSE, angle = 45)
+
+
